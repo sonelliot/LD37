e046a0068c1bd1aac23f1317c2b98c63c39326a4
Elliot Bulmer <sonelliot@gmail.com>
2016-12-11 13:47:51 +0800

    Add recipe and recipe book game object

---
+++ Assets/Scripts/Recipe.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Ingredient
+{
+    Red,
+    Green,
+    Blue,
+    Purple
+}
+
+public class Recipe : MonoBehaviour
+{
+    public string recipeName;
+    public Ingredient[] ingredients;
+}
