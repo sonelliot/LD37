0caf92f17b9c14eef6577a46bc7ce4fed388e68d
Elliot Bulmer <sonelliot@gmail.com>
2016-12-11 18:06:02 +0800

    Basic request items

---
+++ Assets/Scripts/Request.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Request : MonoBehaviour
+{
+    public Recipe recipe;
+    public float timeout = 60;
+
+    public void Update()
+    {
+        for (var i = 0; i < 4; i++)
+        {
+            var renderer = transform.GetChild(i)
+                .gameObject
+                .GetComponent<SpriteRenderer>();
+
+            var ingredient = this.recipe.ingredients
+                .ElementAtOrDefault(i);
+
+            renderer.color = ingredient.GetColor();
+        }
+    }
+}
