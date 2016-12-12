c6dcca0ac50aab505629ae21ce4d02855cffb355
Elliot Bulmer <sonelliot@gmail.com>
2016-12-12 08:29:07 +0800

    Refactor to a `ProgressBar` component

--- Assets/Scripts/CauldronProgress.cs
+++ Assets/Scripts/CauldronProgress.cs
@@ -3,36 +3,27 @@ using System.Collections;
 
 public class CauldronProgress : MonoBehaviour
 {
-    private GameObject m_background;
-    private GameObject m_foreground;
+    private GameObject m_bar;
     private Brewing m_brewing;
 
     public void Start()
     {
         m_brewing = transform.parent.GetComponent<Brewing>();
-        m_foreground = transform.Find("Foreground").gameObject;
-        m_background = transform.Find("Background").gameObject;
+        m_bar = transform.Find("Bar").gameObject;
+
+        var progressBar = m_bar.GetComponent<ProgressBar>();
+        progressBar.percent = () => m_brewing.Progress;
     }
 
     public void Update()
     {
         if (!m_brewing.InProgress)
         {
-            m_foreground.SetActive(false);
-            m_background.SetActive(false);
+            m_bar.SetActive(false);
         }
         else
         {
-            m_foreground.SetActive(true);
-            m_background.SetActive(true);
-
-            var xform = m_foreground.transform;
-            var scale = xform.localScale;
-
-            xform.localScale = new Vector3(
-                Mathf.Clamp(m_brewing.Progress, 0f, 1f),
-                scale.y,
-                scale.z);
+            m_bar.SetActive(true);
         }
     }
 }
---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class ProgressBar : MonoBehaviour
+{
+    private GameObject m_foreground;
+    public Func<float> percent;
+
+    public void Start()
+    {
+        m_foreground = transform.Find("Foreground").gameObject;
+    }
+
+    public void Update()
+    {
+        if (this.percent == null)
+            return;
+
+        var xform = m_foreground.transform;
+        var scale = xform.localScale;
+
+        xform.localScale = new Vector3(
+            Mathf.Clamp(this.percent(), 0f, 1f),
+            scale.y,
+            scale.z);
+    }
+}
