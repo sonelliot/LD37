4231fb0f3e313ebf3653f9e7313924191c89f129
Elliot Bulmer <sonelliot@gmail.com>
2016-12-12 08:35:21 +0800

    Make the request progress bar work

--- Assets/Scripts/Request.cs
+++ Assets/Scripts/Request.cs
@@ -6,9 +6,26 @@ using System.Linq;
 public class Request : MonoBehaviour
 {
     public Recipe recipe;
-    public float timeout = 60;
+    public float timeout = 60f;
+    public float elapsed = 0f;
+
+    public float Progress
+    {
+        get { return this.elapsed / this.timeout; }
+    }
 
     public void Update()
+    {
+        UpdateProgress();
+        UpdateIngredients();
+    }
+
+    private void UpdateProgress()
+    {
+        this.elapsed += Time.deltaTime;
+    }
+
+    private void UpdateIngredients()
     {
         for (var i = 0; i < 4; i++)
         {
---
+++ Assets/Scripts/RequestProgress.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RequestProgress : MonoBehaviour
+{
+    public void Start()
+    {
+        var request = transform.parent
+            .GetComponent<Request>();
+
+        var bar = transform.Find("Bar")
+            .GetComponent<ProgressBar>();
+
+        bar.percent = () => 1f - request.Progress;
+    }
+}
