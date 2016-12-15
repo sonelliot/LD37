0cb5271e19a2e6ecfaab3eec1dd395c54e6fea86
Elliot Bulmer <sonelliot@gmail.com>
2016-12-15 18:13:19 +0800

    Character sprite changes direction based on input

---
+++ Assets/Scripts/Components/OmniSpriteComponent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace WizardWorkshop
+{
+    public class OmniSpriteComponent : MonoBehaviour
+    {
+        // Facing towards the camera.
+        public Sprite down;
+        // Facing away from the camera.
+        public Sprite up;
+        // Facing the left.
+        public Sprite left;
+        // Facing the right.
+        public Sprite right;
+    }
+}
---
+++ Assets/Scripts/Controllers/OmniSpriteController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace WizardWorkshop
+{
+    /// <summary>
+    /// Change the sprite based on movement direction.
+    /// </summary>
+    public class OmniSpriteController : MonoBehaviour
+    {
+        public OmniSpriteComponent omni;
+        public MovementComponent movement;
+        public SpriteRenderer renderer;
+
+        private void Update()
+        {
+            if (movement.direction.sqrMagnitude > 0.0f)
+            {
+                renderer.sprite = ForDirection(movement.direction);
+            }
+        }
+
+        /// <summary>
+        /// Figure out which sprite to use for the movement direction.
+        /// </summary>
+        private Sprite ForDirection(Vector3 direction)
+        {
+            // Character is facing left or right.
+            if (Mathf.Abs(direction.x) > Mathf.Abs(direction.z))
+            {
+                var side = Mathf.Sign(direction.x);
+                return side > 0.0f ? omni.right : omni.left;
+            }
+            // Character is facing forwards or backwards.
+            else
+            {
+                var side = Mathf.Sign(direction.z);
+                return side > 0.0f ? omni.up : omni.down;
+            }
+        }
+    }
+}
